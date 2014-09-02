66600d4943833c63b6bfee8bdbc4ade708ac5fe9
Chris Waters <chris@first65.com>
2014-09-02 08:58:00 +0100

    add plugin file

---
+++ spektrix.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name:        Spektrix
+Plugin URI:         https://github.com/christhesoul/spektrix
+Description:        Integrate Wordpress with Spektrix theatre booking system
+Version:            1.0.0
+Author:             Chris Waters
+Author URI:         http://christhesoul.svbtle.com
+
+License:            MIT License
+License URI:        http://opensource.org/licenses/MIT
+*/
+
+require('vendor/autoload.php');
