d7d3b5229d4574825a58bf602d5aa6f8788e8d5a
Chris Waters <chris@first65.com>
2014-10-03 14:46:51 +0100

    add method with_ids()

--- example.php
+++ example.php
@@ -6,11 +6,4 @@ Dotenv::load(__DIR__);
 
 echo '<pre>';
 $shows = new Spektrix\ShowCollection();
-
-foreach($shows->with_tag('spill festival') as $show){
-  echo '<h1>' . $show->name . '</h1>';
-  $show_id = $show->id;
-}
-
-$spektrix_iframe_url = new Spektrix\iFrame('EventDetails',array('EventId' => $show_id));
-echo $spektrix_iframe_url->render_iframe();
+print_r($shows->with_ids(array(19078,18877)));
--- src/Spektrix/ShowCollection.php
+++ src/Spektrix/ShowCollection.php
@@ -6,23 +6,32 @@ use ArrayObject;
 class ShowCollection extends Base
 {
   public $data;
-  
+
   public function __construct($shows = NULL)
   {
     $events_xml = $this->load_and_clean_xml($shows);
     $this->data = new ArrayObject($this->collect_shows_from_xml($events_xml));
   }
-  
+
   public function with_tag($tag)
   {
     return array_filter($this->data->getArrayCopy(), function($show) use ($tag) { return $show->has_tag($tag); });
   }
-  
+
   public function as_ids()
   {
     return array_keys($this->data->getArrayCopy());
   }
-  
+
+  public function with_ids($array_of_ids)
+  {
+    if(is_array($array_of_ids)){
+      return array_filter($this->data->getArrayCopy(), function($show) use ($array_of_ids) { return in_array($show->id, $array_of_ids); });
+    } else{
+      throw new \Exception('Method with_ids($array) expects argument to be array, got ' . gettype($array_of_ids));
+    }
+  }
+
   private function collect_shows_from_xml($events_xml)
   {
     $shows = array();
@@ -32,7 +41,7 @@ class ShowCollection extends Base
     }
     return $shows;
   }
-  
+
   private function load_and_clean_xml($shows = NULL)
   {
     if(isset($shows)){
@@ -41,7 +50,7 @@ class ShowCollection extends Base
       parent::__construct();
       $events_xml = $this->get_xml_object('events')->Event;
     }
-    $events_xml = property_exists($events_xml, 'Event') ? $events_xml->Event : $events_xml; 
+    $events_xml = property_exists($events_xml, 'Event') ? $events_xml->Event : $events_xml;
     return $events_xml;
   }
 }
